a10786a9258ceb14c6e71e8ea653faee50dc97f1
vasabalint <balint.vasa@gmail.com>
2026-06-20 22:41:47 +0200

    Create Painting.ts

---
+++ Painting.ts
@@ -0,0 +1,11 @@
+export interface Painting {
+  id: string;
+  title: string;
+  year: number;
+  technique: string;
+  dimensions: string;
+  category: string;
+  description: string;
+  image: string;
+  featured: boolean;
+}
